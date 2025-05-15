01383bee0a5f2290f1ed88a3e2c00befa1228979
Lucas Eufrasio <lucaswilliameufrasio@gmail.com>
2025-05-15 13:48:29 -0300

    refactor: move domains to axum state and remove reason parameter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{Json, Router, extract::Query, http::StatusCode, response::IntoResponse, routing::get};
+use axum::{extract::{Query, State}, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use std::{
@@ -6,11 +6,7 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
     net::{IpAddr, Ipv6Addr, SocketAddr},
-    sync::OnceLock,
 };
-
-// Load domains.txt into a HashSet using std::sync::OnceLock
-static DOMAINS: OnceLock<HashSet<String>> = OnceLock::new();
 
 /// Helper to read domains.txt
 fn load_domains(path: &str) -> std::io::Result<HashSet<String>> {
@@ -25,38 +21,9 @@ fn load_domains(path: &str) -> std::io::Result<HashSet<String>> {
     Ok(set)
 }
 
-#[derive(Debug, Deserialize)]
-struct VerifyParams {
-    domain: String,
-}
-
-#[derive(Serialize)]
-struct VerifyResponse {
-    domain: String,
-    is_disposable: bool,
-    reason: Option<String>,
-    source: String,
-    checked_at: String,
-}
-
-async fn verify_handler(Query(params): Query<VerifyParams>) -> impl IntoResponse {
-    let domain_orig = params.domain.clone();
-    let domain = params.domain.to_lowercase();
-    let now = Utc::now().to_rfc3339();
-
-    let set = DOMAINS.get().expect("DOMAINS not initialized");
-    let is_disposable = set.contains(&domain);
-    let reason = is_disposable.then(|| "Listed as disposable".to_string());
-
-    let resp = VerifyResponse {
-        domain: domain_orig,
-        is_disposable,
-        reason,
-        source: "assets/blocklist.txt".into(),
-        checked_at: now,
-    };
-
-    (StatusCode::OK, Json(resp))
+#[derive(Clone)]
+struct AppState {
+    domains: HashSet<String>
 }
 
 #[tokio::main]
@@ -68,18 +35,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = std::env::current_dir()?;
     let path_buffer = cwd.join("assets").join("blocklist.txt");
     let path = path_buffer.to_str().expect("Path not defined properly");
-    
+
     // Initialize DOMAINS once at startup
     let domains = load_domains(path)?;
-    DOMAINS.set(domains).expect("Failed to set domains");
+    let state = AppState {
+        domains,
+    };
 
     let port = std::env::var("PORT").unwrap_or("9999".to_string());
 
     // Build application
-    let app = Router::new().route("/v1/domains/verify", get(verify_handler));
+    let app = Router::new().route("/v1/domains/verify", get(verify_handler)).with_state(state);
 
-    // Run server
-    // Run server on localhost:<port>.
+    // Run server on localhost:<port>
     let address = SocketAddr::from((IpAddr::from(Ipv6Addr::UNSPECIFIED), port.parse()?));
     let listener = tokio::net::TcpListener::bind(address).await?;
 
@@ -88,4 +56,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     axum::serve(listener, app).await?;
 
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct VerifyParams {
+    domain: String,
+}
+
+#[derive(Serialize)]
+struct VerifyResponse {
+    domain: String,
+    is_disposable: bool,
+    source: String,
+    checked_at: String,
+}
+
+async fn verify_handler(State(state): State<AppState>, Query(params): Query<VerifyParams>) -> impl IntoResponse {
+    let domain_orig = params.domain.clone();
+    let domain = params.domain.to_lowercase();
+    let now = Utc::now().to_rfc3339();
+
+    let is_disposable = state.domains.contains(&domain);
+
+    (StatusCode::OK, Json(VerifyResponse {
+        domain: domain_orig,
+        is_disposable,
+        source: "assets/blocklist.txt".into(),
+        checked_at: now,
+    }))
 }
